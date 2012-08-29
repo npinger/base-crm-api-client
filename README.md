78d4756ab75a1a8a5c3bbe21d280181866b5bfbf
Nathan Pinger <npinger@ycharts.com>
2012-08-29 12:16:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Python Interactions with Base
+
+How it works
+============
+
+This client is meant to be as simple to use as possible, mimicking the GET/POST/PUT/DELETE requests documented in the [base api documentation](http://dev.futuresimple.com/api/overview).  Please review that documentation for required parameters 
+
+To set up a connection to base, simply run:
+
+    from base_client import BaseAPIService
+    base_conn = BaseAPIService(email="YOUR_EMAIL", password="YOUR_PASSWORD")
+
+    # This will set up a service that returns json responses.  To return xml, add the argument format='xml'
+
+Then you can start working with base objects, immediately.  Examples (assuming you have instantiated `base_conn` as above).
+
+Examples of getting objects:
+
+    # Return the first page of incoming deals (output will be a json response)
+    base_conn.get_deals(page=1, stage='incoming')
+
+    # Return the first page of contacts (output will be a json response)
+    base_conn.get_contacts(page=1)
+
+Example creation and modification of contact:
+
+    # Create a new contact (json object will be stored as a response)
+    response = base_conn.create_contact(contact_info={'first_name': 'John', 'last_name': 'Doe'})
+
+    # Get the id of the new contact
+    import json
+    response_dict = json.loads(response)
+    contact_id = response_dict['contact']['id']
+
+    # Update the contact's phone number
+    base_conn.update_contact(contact_info={'phone': '555-555-5555'}, contact_id=contact_id)
+
+Important Variables:
+* CONTACT_PARAMS - a dictionary of legal parameters for a contact in the `contact_info` argument.  See the [base api documentation](http://dev.futuresimple.com/api/overview) for required parameters.
+* DEAL_PARAMS - a dictionary of legal parameters for a deal in `deal_info` argument.  See the [base api documentation](http://dev.futuresimple.com/api/overview) for required parameters.
+* DEAL_STAGES - legal options for stage argument in get_deals
+
+Available methods:
+==================
+Please see the code itself for documentation - function headers are reasonably descriptive.  Currently, the module has limited functionality.
+
+Accounts Functions:
+* get_accounts()
+
+Deals Functions:
+* get_deals(page=1, stage='incoming')
+* create_deal(deal_info)
+* update_deal(deal_info, deal_id)
+* create_deal_note(deal_id, note_content)
+* update_deal_note(self, deal_id, note_content, note_id)
+
+Contacts Functions:
+* get_contacts(page=1)
+* create_contact(self, contact_info, person=True)
+* update_contact(self, contact_info, contact_id, person=True)
+* create_contact_note(self, contact_id, note_content)
+* update_contact_note(self, contact_id, note_content, note_id)
+
+Sources Functions:
+* get_sources(self, other=0)
+
+Ongoing Development:
+====================
+This was put together relatively quickly.  There are a couple of areas where improvements will be necessary:
+* Error handling
+* Filling out methods to deal with all types of objects
+
+Feel free to get involved if you want.
---
+++ base_client.py
@@ -0,0 +1,387 @@
+import urllib
+import urllib2
+import logging
+
+import json
+
+logger = logging.getLogger(__name__)
+
+CONTACT_PARAMS = {
+    'name': '',
+    'last_name': '',
+    'first_name': '',
+    'is_organisation': '',
+    'contact_id': '',
+    'email': '',
+    'phone': '',
+    'mobile': '',
+    'twitter': '',
+    'skype': '',
+    'facebook': '',
+    'linkedin': '',
+    'address': '',
+    'city': '',
+    'country': '',
+    'title': '',
+    'description': '',
+    'industry': '',
+    'website': '',
+    'fax': '',
+    'tag_list': '',
+    'private': '',
+}
+
+DEAL_PARAMS = {
+    'name': '',
+    'entity_id': '',
+    'scope': '',
+    'hot': 'false',
+    'deal_tags': '',
+    'contact_ids': '',
+    'source_id': '',
+    'stage': '',
+}
+
+DEAL_STAGES = [
+    'incoming',
+    'qualified',
+    'quote',
+    'custom1',
+    'custom2',
+    'custom3',
+    'closure',
+    'won',
+    'lost',
+    'unqualified',
+]
+
+
+class BaseAPIService(object):
+
+    def __init__(self, email, password, format='json'):
+        """
+        Gets a login token for base, and set the format for response objects.
+        format =    'json' (default)
+                    'xml'
+        """
+        self.base_url = 'https://sales.futuresimple.com/api/v1/'
+
+        if format == 'json':
+            self.format = '.json'
+        elif format == 'xml':
+            self.format = '.xml'
+
+        # Get token
+        status, self.token = self._get_login_token(email=email, password=password)
+        if status == "ERROR":
+            # If we get an error, return it.
+            logger.error(self.token)
+            self.auth_failed = True
+        else:
+            # Set URL header for future requests
+            self.header = {"X-Pipejump-Auth": self.token}
+            self.auth_failed = False
+
+    ##########################
+    # Token Functions
+    ##########################
+    def _get_login_token(self, email, password):
+        """
+        Passes email and password to base api and returns login token.
+        """
+        auth_url = 'authentication.json'
+        params = urllib.urlencode({
+            'email': email,
+            'password': password,
+        })
+
+        try:
+            data = urllib2.urlopen(self.base_url + auth_url, params).read()
+        except urllib2.HTTPError, e:
+            return ("ERROR", "HTTP: %s" % str(e))
+        except urllib2.URLError, e:
+            return ("ERROR", "Error URL: %s" % str(e.reason.args[1]))
+
+        try:
+            dict_data = json.loads(data)
+            token = dict_data["authentication"]["token"]
+        except KeyError:
+            return ("ERROR", "Error: No Token Returned")
+
+        return ("SUCCESS", token)
+
+    ##########################
+    # Accounts Functions
+    ##########################
+    def get_accounts(self):
+        """
+        Get current account.
+        """
+        account_url = 'account' + self.format
+
+        url = self.base_url + account_url
+
+        req = urllib2.Request(url, headers=self.header)
+        response = urllib2.urlopen(req)
+        data = response.read()
+
+        return data
+
+    ##########################
+    # Deals Functions
+    ##########################
+    def get_deals(self, page=1, stage='incoming'):
+        """
+        Gets deal objects in batches of 20.
+        Arguments:
+            page = the set of deals to return. 1 (default) returns the first 20.
+            stage = the stage of deals to return - see DEAL_STAGES list for details.
+        """
+        deals_url = 'deals' + self.format
+        url = self.base_url + deals_url
+        params = urllib.urlencode({
+            'page': page,
+            'stage': stage,
+        })
+
+        full_url = url + '?' + params
+
+        req = urllib2.Request(full_url, headers=self.header)
+        response = urllib2.urlopen(req)
+        data = response.read()
+
+        return data
+
+    def create_deal(self, deal_info):
+        """
+        Creates a new deal in base, given proper deal parameters in deal_info argument.
+        Proper parameters are shown in the DEAL_PARAMS dictionary.
+        Returns a json or xml reponse.
+        """
+        return self._post_deal(deal_info=deal_info)
+
+    def update_deal(self, deal_info, deal_id):
+        """
+        Updates a deal with the given the base deal_id accoring to deal_info parameters.
+        Proper parameters are shown in the DEAL_PARAMS dictionary.
+        Returns a json or xml response.
+        """
+        return self._post_deal(deal_info=deal_info, deal_id=deal_id)
+
+    def _post_deal(self, deal_info={}, deal_id=None):
+        """
+        PRIVATE FUNCTION:
+        Creates a new deal if deal_id = None.
+        Otherwise, edits the deal with the given deal_id.
+        """
+
+        deals_url = 'deals'
+        if deal_id != None:
+            deals_url += '/%s' % str(deal_id)
+        deals_url += self.format
+
+        url = self.base_url + deals_url
+
+        if deal_info == {} or (('name' not in deal_info.keys() \
+                or 'entity_id' not in deal_info.keys()) and deal_id == None):
+            return "Missing required attributes 'name' or 'entity_id'"
+
+        final_params = {}
+
+        for key in deal_info.keys():
+            if key not in DEAL_PARAMS.keys():
+                return "%s is not a legal deal attribute" % key
+            else:
+                final_params[key] = deal_info[key]
+
+        params = urllib.urlencode(final_params)
+
+        req = urllib2.Request(url, data=params, headers=self.header)
+        if deal_id != None:
+            req.get_method = lambda: 'PUT'
+        response = urllib2.urlopen(req)
+        data = response.read()
+
+        return data
+
+    def create_deal_note(self, deal_id, note_content):
+        """
+        Creates a note associated with a specific deal (defined by Base's unique deal_id)
+        with the content note_content.
+        Returns a json or xml response.
+        """
+        return self._post_deal_note(deal_id=deal_id, note_content=note_content)
+
+    def update_deal_note(self, deal_id, note_content, note_id):
+        """
+        Edits a note (defined by Base's unique deal_id and the note's unique note_id)
+        with the content note_content.
+        Returns a json or xml response.
+        """
+        return self._post_deal_note(deal_id=deal_id, note_content=note_content, note_id=note_id)
+
+    def _post_deal_note(self, deal_id, note_content='', note_id=None):
+        """
+        PRIVATE FUNCTION
+        Creates a new note for a given deal_id with content note_content, if note_id == None.
+        Otherwise, edits the note with the given note_id.
+        """
+
+        url_base_template = 'deals/%s/notes' % str(deal_id)
+        if note_id != None:
+            url_base_template += '/%s' % str(note_id)
+        url_base_template += self.format
+
+        url = self.base_url + url_base_template
+
+        params = urllib.urlencode({'note[content]': note_content})
+
+        req = urllib2.Request(url, data=params, headers=self.header)
+        if note_id != None:
+            req.get_method = lambda: 'PUT'
+        response = urllib2.urlopen(req)
+        data = response.read()
+
+        return data
+
+    ##########################
+    # Contact Functions
+    ##########################
+    def get_contacts(self, page=1):
+        """
+        Gets contact objects.
+        """
+        contacts_url = 'contacts' + self.format
+        url = self.base_url + contacts_url
+        params = urllib.urlencode({
+            'page': page,
+        })
+        # Append parameters
+        full_url = url + '?' + params
+
+        req = urllib2.Request(full_url, headers=self.header)
+        response = urllib2.urlopen(req)
+        data = response.read()
+
+        return data
+
+    def create_contact(self, contact_info, person=True):
+        """
+        Creates a new contact based on contact_info with fields shown in CONTACT_PARAMS.
+        Assumes the contact is a person.  If the contact is a company, use person=False
+        Returns a json or xml response.
+        """
+        return self._post_contact(contact_info=contact_info, contact_id=None, person=person)
+
+    def update_contact(self, contact_info, contact_id, person=True):
+        """
+        Edits contact with the unique base_id based on contact_info with fields shown in CONTACT_PARAMS.
+        Assumes the contact is a person.  If the contact is a company, use person=False
+        Returns a json or xml response.
+        """
+        return self._post_contact(contact_info=contact_info, contact_id=contact_id, person=person)
+
+    def _post_contact(self, contact_info={}, contact_id=None, person=True):
+        """
+        Creates a new contact if contact_id == None.
+        Otherwise, edits contact with the given id.
+        """
+        contacts_url = 'contacts'
+        if contact_id != None:
+            contacts_url += '/%s' % str(contact_id)
+        contacts_url += self.format
+
+        url = self.base_url + contacts_url
+
+        CONTACT_PARAMS['is_organisation'] = 'false'
+        if not person:
+            CONTACT_PARAMS['is_organisation'] = 'true'
+
+        if contact_info == {} or \
+                ('name' not in contact_info.keys() and 'last_name' not in contact_info.keys()):
+            return
+
+        final_params = {}
+
+        for key in contact_info.keys():
+            if key not in CONTACT_PARAMS.keys():
+                return
+            else:
+                final_params['contact[' + key + ']'] = contact_info[key]
+
+        params = urllib.urlencode(final_params)
+
+        req = urllib2.Request(url, data=params, headers=self.header)
+
+        if contact_id != None:
+            req.get_method = lambda: 'PUT'
+        response = urllib2.urlopen(req)
+        data = response.read()
+
+        return data
+
+    def create_contact_note(self, contact_id, note_content):
+        """
+        Creates a note associated with a specific contact (defined by Base's unique contact_id)
+        with the content note_content.
+        Returns a json or xml response.
+        """
+        return self._post_contact_note(contact_id=contact_id, note_content=note_content)
+
+    def update_contact_note(self, contact_id, note_content, note_id):
+        """
+        Edits a note (defined by Base's unique contact_id and the note's unique note_id)
+        with the content note_content.
+        Returns a json or xml response.
+        """
+        return self._post_contact_note(contact_id=contact_id, note_content=note_content, note_id=note_id)
+
+    def _post_contact_note(self, contact_id, note_content='', note_id=None):
+        """
+        PRIVATE FUNCTION
+        Creates a new note for a given contact_id with content note_content, if note_id == None.
+        Otherwise, edits the note with the given note_id.
+        """
+
+        url_base_template = 'contacts/%s/notes' % str(contact_id)
+        if note_id != None:
+            url_base_template += '/%s' % str(note_id)
+        url_base_template += self.format
+
+        url = self.base_url + url_base_template
+
+        params = urllib.urlencode({'note[content]': note_content})
+
+        req = urllib2.Request(url, data=params, headers=self.header)
+        if note_id != None:
+            req.get_method = lambda: 'PUT'
+        response = urllib2.urlopen(req)
+        data = response.read()
+
+        return data
+
+    ##########################
+    # Sources Functions
+    ##########################
+    def get_sources(self, other=0):
+        """
+        Gets deal sources.
+        Argument:
+            other: default to 0.  If 1, retrieves sources added by other users in the account.
+        """
+        sources_url = 'sources' + self.format
+        url = self.base_url + sources_url
+        if other == 1:
+            params = urllib.urlencode({
+                'other': other,
+            })
+        else:
+            params = ''
+        # Append parameters
+        full_url = url + '?' + params
+
+        req = urllib2.Request(full_url, headers=self.header)
+        response = urllib2.urlopen(req)
+        data = response.read()
+
+        return data
